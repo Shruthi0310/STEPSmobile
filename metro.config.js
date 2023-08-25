735f989912c768df9ed93796fb80b25aa4947c03
shruthi <e0564271@u.nus.edu>
2023-08-25 16:01:54 +0800

    chore: update dependencies

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,21 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
+// /** @type {import('expo/metro-config').MetroConfig} */
+// const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+// module.exports = config;
+
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts }
+  } = await getDefaultConfig(__dirname);
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve("react-native-sass-transformer")
+    },
+    resolver: {
+      sourceExts: [...sourceExts, "scss", "sass"]
+    }
+  };
+})();
